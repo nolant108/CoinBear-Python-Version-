39ebbfc918f48021885cd29f8cd96adf75df9f4e
Nolan Trapp <officialstackemup@gmail.com>
2020-10-12 12:31:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
 const SHA256 = require('crypto-js/sha256');
 
-class Block{
-    constructor(index, timestamp, data, previousHash = ''){
-        this.index = index;
-        this.previousHash = previousHash;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.hash = this.calculateHash;
-        this.nonce = 0;
+class Block {
+    constructor(index, timestamp, data, previousHash) {
+     this.index = index;
+     this.timestamp = timestamp;
+     this.data = data;
+     this.previousHash = previousHash;
+     this.nonce = 0;
+     this.hash = this.calculateHash();
     }
-
-    calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();   
+   
+    calculateHash() {
+     return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce);
     }
-
-
-    mineBlock(difficulty){
-        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')){
-            this.nonce++;
-            this.hash = this.calculateHash();
-        }
-
-        console.log("Block mined: " + this.hash);
-    }  
-}
+   
+    mineBlock(difficulty) {
+     while (this.hash.toString().substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+     }
+     console.log("Block Successfully hashed: " + this.hash);
+    }
+   }
 
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
-        this.difficulty = 2;
+        this.difficulty = 5;
     }
 
     createGenesisBlock(){
